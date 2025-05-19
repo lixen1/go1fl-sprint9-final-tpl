5a733f7eabfdf7f956e43eeae241431c16ca88b8
Egor <Lixen1@protonmail.com>
2025-05-20 00:24:37 +0300

    first-commit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +14,90 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 {
+		return []int{}
+	}
+
+	s := make([]int, size)
+
+	for i := range s {
+		s[i] = rand.Int()
+	}
+
+	return s
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	} else if len(data) == 1 {
+		return data[0]
+	}
+	maxNum := 0
+	for i := range data {
+		if data[i] > maxNum {
+			maxNum = data[i]
+		}
+	}
+	return maxNum
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	} else if len(data) == 1 {
+		return data[0]
+	}
+
+	var wg sync.WaitGroup
+	sizeOfSlice := SIZE / CHUNKS
+	maxInSlices := make([]int, CHUNKS)
+
+	wg.Add(CHUNKS)
+
+	for i := 0; i < CHUNKS; i++ {
+		initalIndex := i * sizeOfSlice
+
+		go func(slice []int) {
+			defer wg.Done()
+			maxNum := 0
+
+			for j := range slice {
+				if slice[j] > maxNum {
+					maxNum = slice[j]
+				}
+			}
+			maxInSlices[i] = maxNum
+		}(data[initalIndex : initalIndex+sizeOfSlice])
+	}
+	wg.Wait()
+
+	totalMax := 0
+	for i := range maxInSlices {
+		if maxInSlices[i] > totalMax {
+			totalMax = maxInSlices[i]
+		}
+	}
+	return totalMax
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	data := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	start := time.Now()
+	max := maximum(data)
+	elapsed := (time.Since(start)).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max = maxChunks(data)
+	elapsed = (time.Since(start)).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,76 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+
+	t.Run("Zero size", func(t *testing.T) {
+		res := generateRandomElements(0)
+		if len(res) != 0 {
+			t.Errorf("Want empty slice, have %d", len(res))
+		}
+	})
+
+	t.Run("Negative size", func(t *testing.T) {
+		res := generateRandomElements(-35)
+		if len(res) != 0 {
+			t.Errorf("Want empty slice, have %d", len(res))
+		}
+	})
+	t.Run("Positive size", func(t *testing.T) {
+		size := 10
+		result := generateRandomElements(size)
+		if len(result) != size {
+			t.Errorf("Want %d, have %d", size, len(result))
+		}
+	})
+}
+
+func TestMaximum(t *testing.T) {
+	t.Run("Empty slice", func(t *testing.T) {
+		result := maximum([]int{})
+		if result != 0 {
+			t.Errorf("Want zero, have %d", result)
+		}
+	})
+
+	t.Run("Single element in slice", func(t *testing.T) {
+		result := maximum([]int{3242})
+		if result != 3242 {
+			t.Errorf("Want 3242, have %d", result)
+		}
+	})
+
+	t.Run("Multiple elements", func(t *testing.T) {
+		result := maximum([]int{1, 5, 3, 9, 2})
+		if result != 9 {
+			t.Errorf("Want max = 9, have %d", result)
+		}
+	})
+
+	t.Run("All negative elements", func(t *testing.T) {
+		result := maximum([]int{-10, -3, -50})
+		if result != 0 {
+			t.Errorf("Want zero, have %d", result)
+		}
+	})
+}
+
+func TestMaxChunks(t *testing.T) {
+	t.Run("Empty slice", func(t *testing.T) {
+		result := maxChunks([]int{})
+		if result != 0 {
+			t.Errorf("Want zero, have %d", result)
+		}
+	})
+
+	t.Run("Single element in slice", func(t *testing.T) {
+		result := maxChunks([]int{238})
+		if result != 238 {
+			t.Errorf("Want 238, have %d", result)
+		}
+	})
+
+}
